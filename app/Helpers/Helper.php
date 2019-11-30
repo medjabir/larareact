9fd0b4f5567dae228f83427e21e8104f41bb6706
Mohammed Jabir <med.jabir@gmail.com>
2019-11-30 20:08:50 +0100

    Refactor the code

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,16 +4,16 @@ namespace App\Helpers;
 
 class Helper {
 
-        public static function normalized_email(string $email): string
-        {
-            list($user, $provider) = explode('@', $email);
+    public static function normalized_email(string $email): string
+    {
+        list($user, $provider) = explode('@', $email);
     
-            $user = explode('+', $user)[0];
-            $user = str_replace('.', '', strtolower($user));
+        $user = explode('+', $user)[0];
+        $user = str_replace('.', '', strtolower($user));
     
-            $provider = strtolower($provider);
+        $provider = strtolower($provider);
     
-            return $user . '@' . $provider;
-        }
+        return $user . '@' . $provider;
+    }
     
 }
